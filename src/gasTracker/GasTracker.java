a84a43a3bb3c26d5f5b612aae278543dc8381585
daryusl <dl_199@hotmail.com>
2019-05-07 02:55:17 -0700

    Commit Test

--- src/gasTracker/GasTracker.java
+++ src/gasTracker/GasTracker.java
@@ -5,5 +5,7 @@ import java.util.ArrayList;
 public class GasTracker {
 	private ArrayList<Entry> entires = new ArrayList<>();
 	
-	
+	public GasTracker() {
+		System.out.println("What is your current KM on car?");
+	}
 }
