b788b939794fea2bc6e87a6103d908b1b0e185be
daryus <f6z1b>
2019-05-11 16:53:48 -0700

    added functionality for calculating the average fuel consumption

--- src/gasTracker/Entry.java
+++ src/gasTracker/Entry.java
@@ -1,5 +1,37 @@
 package gasTracker;
 
+import java.util.ArrayList;
+
 public class Entry {
 
+    private int currKM;
+    private int prevKM;
+    private float cost;
+    private float litres;
+
+    public Entry(int currKM, int prevKM, float cost, float litres) {
+        this.currKM = currKM;
+        this.prevKM = prevKM;
+        this.cost = cost;
+        this.litres = litres;
+
+    }
+
+    protected float printAvg(ArrayList<Entry> entries) {
+        float entryCost = 0;
+        for (int i = 0; i < entries.size(); i++) {
+            entryCost = entryCost + entries.get(i).cost;
+        }
+        int size = entries.size();
+        entryCost = entryCost / size;
+        return entryCost;
+    }
+
+    protected float printConsumption(ArrayList<Entry> entries) {
+        float entryConsumption = 0;
+        for (int i = 0; i < entries.size(); i++) {
+
+        }
+        return 0;
+    }
 }
--- src/gasTracker/GasTracker.java
+++ src/gasTracker/GasTracker.java
@@ -1,11 +1,38 @@
 package gasTracker;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GasTracker {
-	private ArrayList<Entry> entires = new ArrayList<>();
-	
-	public GasTracker() {
-		System.out.println("What is your current KM on car?");
-	}
-}
+    private ArrayList<Entry> entries = new ArrayList<>();
+    private int done = 0;
+
+    public GasTracker() {
+        while (done == 0) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is your current KM on car?");
+            String KM = input.nextLine();
+            int currKM = Integer.parseInt(KM);
+            System.out.println("What is the cost of your fuel?");
+            String c = input.nextLine();
+            float cost = Float.parseFloat(c);
+            System.out.println("How many litres of gas did you fill?");
+            String l = input.nextLine();
+            float litres = Float.parseFloat(l);
+            Entry entry = new Entry(currKM, 0, cost, litres); // set up the entry
+
+            // want to add object to arraylist
+            entries.add(entry);
+
+            // print the avg of fuel cost
+            System.out.println("Your fuel cost average is: ");
+            System.out.println(entry.printAvg(entries));
+
+            // print avg price/L
+            System.out.println("Your avg L/100KM is: ");
+
+            }
+        }
+    }
+
+
