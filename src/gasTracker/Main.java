ad6038ba6e5875d3fa6c1c4cf3544b95538f8755
daryus <f6z1b>
2019-05-28 18:57:38 -0700

    added functionality

--- src/gasTracker/Main.java
+++ src/gasTracker/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GasTracker app = new GasTracker();
-
+		System.out.println("Gas has been tracked.");
 	}
 
 }
